fe642006f5d9a319368bd68332aa26d94714bced
Xinran-Ma <xinran_ma@hotmail.com>
2020-05-09 22:37:16 +1000

    Improved the performance of MorderSummary and Modal components.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentDidUpdate () {
+        console.log('[OrderSummary] DidUpdate')
+    }
+    
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}:</span> {props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}:</span> {this.props.ingredients[igKey]}
                 </li>
             )
         });
-    
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price:</strong> {props.price.toFixed(2)}</p>
-            <p>Continue to checkout?</p>
-            <Button clicked={props.purchaseCancelled} btnType="Danger">CANCEL</Button>
-            <Button clicked={props.purchaseContinuedHandler} btnType="Success">CONTINUE</Button>
-        </Aux>
-    )
-};
 
-export default orderSummary;
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price:</strong> {this.props.price.toFixed(2)}</p>
+                <p>Continue to checkout?</p>
+                <Button clicked={this.props.purchaseCancelled} btnType="Danger">CANCEL</Button>
+                <Button clicked={this.props.purchaseContinuedHandler} btnType="Success">CONTINUE</Button>
+            </Aux>
+        )
+    }
+        
+    
+}
+
+
+
+export default OrderSummary;

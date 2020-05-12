f055a63c05764ac5357867e154c868677c7b6a0b
Xinran-Ma <xinran_ma@hotmail.com>
2020-05-12 23:28:44 +1000

    Retrieve data from the backend (Firebase).

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,17 +21,22 @@ class BurgerBuilder extends Component {
     //     this.state = {}
     // }
     state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
-
+        ingredients: null,
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('https://react-my-burger-260b0.firebaseio.com/ingredients.json')
+            .then(res => {
+                this.setState({ingredients: res.data})
+            })
+            .catch(error => {
+                this.setState({error: true})
+            })
     }
 
     updatePurchaseState (ingredients) {
@@ -119,29 +124,47 @@ class BurgerBuilder extends Component {
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0;
         }
-        let orderSummary = <OrderSummary 
+
+        let orderSummary = null;
+
+        if(this.state.loading) {
+            orderSummary = <Spinner />
+        }
+
+        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
+
+        if(this.state.ingredients) {
+            burger = (
+                <Aux>
+                    <Burger ingredients={this.state.ingredients} />
+                    <BuildControls 
+                        ingredientAdded={this.addIngredientHandler}
+                        ingredientRemoved={this.removeIngredientHandler}
+                        disabled={disabledInfo}
+                        purchasable={this.state.purchasable}
+                        price={this.state.totalPrice}
+                        ordered={this.purchaseHandler}
+                    />
+                </Aux>
+            );
+
+            orderSummary = <OrderSummary 
                                 ingredients={this.state.ingredients}
                                 price={this.state.totalPrice}
                                 purchaseCancelled={this.purchaseCancelHandler}
                                 purchaseContinuedHandler={this.purchaseContinueHandler} />
-        if(this.state.loading) {
+        }
+
+        if (this.state.loading) {
             orderSummary = <Spinner />
         }
+
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
-                    price={this.state.totalPrice}
-                    ordered={this.purchaseHandler}
-                />
-                
+                {burger}
             </Aux>
         );
     }
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,10 +4,13 @@ import Aux from '../Aux/Aux'
 
 const withErrorHandler = ( WrappedComponent, axios ) => {
     return class extends Component {
-        state = {
-            error: null
-        }
-        componentDidMount () {
+
+        constructor(props) {
+            super(props);
+            this.state = {
+                error: null
+            }
+
             axios.interceptors.request.use(req => {
                 this.setState({error: null})
                 return req;
@@ -16,6 +19,20 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
                 this.setState({error: error})
             })
         }
+
+        // state = {
+        //     error: null
+        // }
+
+        // componentWillMount () {
+        //     axios.interceptors.request.use(req => {
+        //         this.setState({error: null})
+        //         return req;
+        //     })
+        //     axios.interceptors.response.use(res => res, error => {
+        //         this.setState({error: error})
+        //     })
+        // }
 
         errorConfirmedHandler = () => {
             this.setState({error: null})

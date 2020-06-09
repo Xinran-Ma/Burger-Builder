c958084028cf5b95e4ac759f605e13589541dc9c
Xinran-Ma <xinran_ma@hotmail.com>
2020-06-09 22:29:31 +1000

    Applied redux thunk to prepare for adding asynchronous code.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,13 +25,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount() {
         console.log(this.props);
-        // axios.get('https://react-my-burger-260b0.firebaseio.com/ingredients.json')
-        //     .then(res => {
-        //         this.setState({ingredients: res.data})
-        //     })
-        //     .catch(error => {
-        //         this.setState({error: true})
-        //     })
+        axios.get('https://react-my-burger-260b0.firebaseio.com/ingredients.json')
+            .then(res => {
+                this.setState({ingredients: res.data})
+            })
+            .catch(error => {
+                this.setState({error: true})
+            })
     }
 
     updatePurchaseState (ingredients) {

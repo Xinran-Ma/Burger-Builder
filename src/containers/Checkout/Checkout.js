25b9e5ad4e6c41203901c6cbc14f601f194a5ade
Xinran-Ma <xinran_ma@hotmail.com>
2020-06-13 23:34:34 +1000

    Handling purchases and updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -16,9 +16,12 @@ class Checkout extends Component {
 
     render () {
         let summary = <Redirect to="/" />
+        
         if(this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to='/' /> : null;
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary 
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -48,8 +51,9 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 }
 
-export default connect(mapStateToProps)(Checkout);
+export default connect( mapStateToProps )(Checkout);

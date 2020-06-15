c50afec92b3de430cd4b479a2c6bcd4059a9532e
Xinran-Ma <xinran_ma@hotmail.com>
2020-06-16 00:10:30 +1000

    Added authentication form.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -125,6 +125,16 @@ class ContactData extends Component {
             isValid = value.length <= rules.maxLength && isValid
         }
 
+        if(rules.isEmail) {
+            const pattern = /[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)/
+            isValid = pattern.test(value) && isValid
+        }
+
+        if(rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
         return isValid;
     }
 

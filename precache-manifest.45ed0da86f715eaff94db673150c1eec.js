04bf190e43f6652d98a948b4f1aad14069c72d39
Xinran-Ma <xinran_ma@hotmail.com>
2020-06-17 00:45:25 +1000

    Updates

---
+++ precache-manifest.45ed0da86f715eaff94db673150c1eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e38137074297b8120fb8518007d7fc",
+    "url": "/Burger-Builder/index.html"
+  },
+  {
+    "revision": "7037ecc84424e2775671",
+    "url": "/Burger-Builder/static/css/main.214d6a23.chunk.css"
+  },
+  {
+    "revision": "b319f247f94b9a306ef9",
+    "url": "/Burger-Builder/static/js/2.f3e974a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Burger-Builder/static/js/2.f3e974a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7037ecc84424e2775671",
+    "url": "/Burger-Builder/static/js/main.21d72b24.chunk.js"
+  },
+  {
+    "revision": "44b0438f11e99a9274b7",
+    "url": "/Burger-Builder/static/js/runtime-main.b64afb4c.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder/static/media/burger-logo.b8503d26.png"
+  }
+]);

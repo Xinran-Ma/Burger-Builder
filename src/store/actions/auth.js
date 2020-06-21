a3032ac1e9a10e0007e72ec851d78a8fef65a4c5
Xinran-Ma <xinran_ma@hotmail.com>
2020-06-21 23:18:38 +1000

    Stored the token from axios response from the firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,17 +7,18 @@ export const authStart = () => {
     }
 }
 
-export const authSuccess = (authData) => {
+export const authSuccess = (idToken, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: idToken,
+        userId: userId,
     }
 }
 
 export const authFail = (error) => {
     return {
         type: actionTypes.AUTH_FAIL,
-        error: error
+        error: error,
     }
 }
 
@@ -37,7 +38,7 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
-                dispatch(authSuccess(response.data))
+                dispatch(authSuccess(response.data.idToken, response.data.localId))
             })
             .catch(err => {
                 console.log(err);
